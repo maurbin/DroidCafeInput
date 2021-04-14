99bcbea76cf7fcf3806097619783512e7482b555
maurbin <Diosmequiere0>
2021-04-14 17:54:34 +0200

    .

--- app/src/main/java/com/example/droidcafe/MainActivity.java
+++ app/src/main/java/com/example/droidcafe/MainActivity.java
@@ -43,17 +43,24 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_order:
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+                return true;
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_favorites_message));
+                return true;
+            case R.id.action_content:
+                displayToast(getString(R.string.action_contact_message));
+                return true;
+            default:
         }
-
         return super.onOptionsItemSelected(item);
+
     }
 
     public void displayToast(String message) {
